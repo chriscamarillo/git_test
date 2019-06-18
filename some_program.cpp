6781fccd905ccf0969f08571c79ad4575a2216b0
Chris <ccamarillo32298@gmail.com>
2019-06-18 19:53:46 -0400

    created a new branch

--- some_program.cpp
+++ some_program.cpp
@@ -1,6 +1,5 @@
 int main() {
-	// oh look an original comment from the original file
-	// some comment from git_test2
+	// comments are unnecesarry
 	printf("oh look I'm on github rn");
 	return 0;
 }
